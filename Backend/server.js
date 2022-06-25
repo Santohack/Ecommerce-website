f3df4d8610dfbf7373cb48846626e583b5ec056e
santohack <santohack238@gmail.com>
2022-06-25 15:07:47 +0530

    axios

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const app = express();
-app.listen(3000, () => console.log("Server started on port 3000"));
+const products = require("./data/products");
+app.listen(300, () => console.log("Server started on port 300"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
