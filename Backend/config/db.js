cbd57cfbd6ddce0a5b89182c47a45c9555626035
santohack <santohack238@gmail.com>
2022-06-30 17:05:42 +0530

    mongoose

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,14 +1,20 @@
-import mongoose from 'mongoose';
-const  connectDb = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-    } catch (error) {
-        console.log(`Error: ${error.message}`;
-        process.exit(1);
-    }
-}
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://santohack:santohack@shopping.x5ppwou.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.green.underline.bold
+    );
+  } catch (error) {
+    console.log(`Error: ${error}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+export default connectDb;
--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,19 @@
+import colors from "colors";
+import connectDb from "./config/db.js";
 import dotenv from "dotenv";
 import express from "express";
 import products from "./data/Products.js";
 
 const app = express();
+connectDb();
 
-dotenv.config();
+dotenv.config({ path: "./config/.env" });
 const PORT = process.env.PORT || 300;
 app.listen(PORT, () =>
-  console.log(`Server  started running ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server  started running ${process.env.NODE_ENV} on port ${PORT}`.yellow
+      .underline.bold
+  )
 );
 
 app.get("/", (req, res) => {
