3d6b09dbd260fc9a67839f50d98bf8390c762967
santohack <santohack238@gmail.com>
2022-07-07 10:26:55 +0530

    mongo finished

--- Backend/server.js
+++ Backend/server.js
@@ -2,27 +2,32 @@ import colors from "colors";
 import connectDb from "./config/db.js";
 import dotenv from "dotenv";
 import express from "express";
-import products from "./data/Products.js";
+import productsRoutes from "./routes/productsRoutes.js";
 
 const app = express();
 connectDb();
-
+// app.use((req, res, next) => {
+//   const err = new Error(`Not Found - ${req.originalUrl}`);
+//   err.status = 404;
+//   next(err);
+// });
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+app.use((err, req, res, next) => {
+  const statusCode = req.statusCode === 200 ? 500 : req.statusCode;
+  res.status(statusCode).json({
+    message: err.message,
+  });
+});
 dotenv.config({ path: "./config/.env" });
 const PORT = process.env.PORT || 300;
 app.listen(PORT, () =>
-  console.log(
-    `Server  started running ${process.env.NODE_ENV} on port ${PORT}`.yellow
-      .underline.bold
-  )
+  console.log(`Server  started running  on port ${PORT}`.yellow.underline.bold)
 );
+app.use("/api/products", productsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
-});
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
 });
